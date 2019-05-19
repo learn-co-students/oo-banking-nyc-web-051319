069e3b37ce12591b415156965032686b1bbfb5a6
Yana <Yana.Loginova@cix.csi.cuny.edu>
2019-05-19 19:10:36 -0400

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,35 @@
 class BankAccount
 
+attr_accessor :balance, :status
+attr_reader :name
+
+@@all=[]
+
+def initialize(name)
+  @name, @balance, @status = name, 1000, "open"
+  @@all<<self
+end
+
+def deposit(money)
+@balance+=money
+
+end
+
+def display_balance
+   "Your balance is $#{balance}."
+end
+
+def valid?
+ if status==="open" && balance>0
+    return true
+  else
+    false
+ end
+end
+
+def close_account
+  self.status="closed"
+
+end
+
 end
