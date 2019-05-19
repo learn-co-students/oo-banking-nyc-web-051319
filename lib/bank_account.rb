a4564c0730795019c9da781be74dbb692003abdb
Elma Rahman <elmarahman635@gmail.com>
2019-05-19 19:06:03 -0400

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,39 @@
 class BankAccount
+attr_accessor  :balance, :status
+attr_reader :name
+
+@@all = []
+
+def initialize(name)
+  @name = name
+  @balance = 1000
+  @status = 'open'
+
+  @@all << self
+end
+
+def self.all
+  @@all
+end
+
+def deposit(deposit_amount)
+  @balance += deposit_amount #adding deposit_amount to balance
+end
+
+def display_balance
+  "Your balance is $#{@balance}."  #doing self.balance is equivalent to this bc @ is indicating the class of BankAccount
+end
+
+def valid?
+  self.status == "open" && balance > 0
+#status is already set to open so confirm that it's set to open & greater than 0
+end
+
+def close_account
+   self.status = 'closed' #based off spec just set it to close 
+end
+
+
+
 
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,66 @@
 class Transfer
-  # your code here
+  attr_accessor :receiver, :sender, :status, :amount
+  attr_reader :name
+
+  @@all = []
+
+def initialize(sender, receiver, amount) #put it in the correct order bc it'll be in ref to spec
+  @name = name
+  @status = 'pending'
+  @sender = sender
+  @receiver = receiver
+  @amount = amount
+
+  @@all << self
+end
+
+def valid?
+  sender.valid? #returns true or false
+  receiver.valid?  #returns true or false
+#we're testing status & balance from bank account -- if status is open and balance is 0 = true ** the info will be sent back to transfer
+end
+
+def self.all
+  @@all
+end
+
+def execute_transaction
+    if sender.balance >= @amount && @status == "pending"
+      self.status = "complete"
+        receiver.balance += self.amount
+          sender.balance -= self.amount
+        else
+          self.status = "rejected"
+          "Transaction rejected. Please check your account balance."
+
+        end
+      end
+
+
+#use @ more
+
+    # if self.amount > 0 #bc transfer doesnt have balance, it has amount -- so amount is  the amount we're sending eachother -- so if it's greater than 0 its successful
+    #
+    #       self.status = 'complete'
+    #         else
+    #           puts "Transaction rejected. Please check your account balance."
+    #         end
+
+
+
+  # if sender.balance = @balance
+  # 'complete'
+  #   else receiver.balance =
+  #   'rejected'
+  #
+
+  def reverse_transfer
+    if self.status == "complete"
+      receiver.balance -= self.amount
+      sender.balance += self.amount #reversing transfer so getting money back into account.
+      self.status = "reversed"
+
+  end
+end
+
 end
